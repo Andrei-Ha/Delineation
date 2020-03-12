9be8ea4b7e7b8c371a30bd4a5a4ed04101c3cf38
Андрей Сергеич <asgoreglyad@gmail.com>
2020-03-12 16:50:50 +0300

    before add migration D_Tc

--- Delineation/Models/D_res.cs
+++ Delineation/Models/D_res.cs
@@ -10,8 +10,10 @@ namespace Delineation.Models
 {
     public class D_Res
     {
-        public int ID { get; set; }
+        public int ID { get; }
+        [Display(Name="Код")]
         public int Code { get; set; }
+        [Display(Name ="Название")]
         public string Name { get; set; }
         public D_Person Nach { get; set; }
         public int? NachId { get; set; }
@@ -24,12 +26,39 @@ namespace Delineation.Models
     }
     public class D_Person
     {
-        public int Id { get; set; }
+        public int Id { get; }
+        [Display(Name ="Фамилия")]        
         public string Surname { get; set; }
+        [Display(Name="Имя")]
         public string Name { get; set; }
+        [Display(Name="Отчество")]
         public string Patronymic { get; set; }
         [NotMapped]
         public string FIO { get; set; }
+    }
+    public class D_Tc
+    {
+        public int Id { get; }
+        [Display(Name = "№ ТУ")]
+        [Column(TypeName = "varchar(20)")]
+        public string Num { get; set; }
+        [Display(Name = "Дата выдачи")]
+        public DateTime Date { get; set; }
+        [Display(Name = "ФИО заявителя")]
+        public string FIO { get; set; }
+        [Display(Name = "РЭС")]
+        public D_Res Res { get; set; }
+        public int? ResId { get; set; }
+        [Display(Name = "Адрес объекта строительства")]
+        public string Address { get; set; }
+        [Display(Name = "Разрешенная мощность")]
+        public double Pow { get; set; }
+        [Display(Name = "Категория")]
+        public int Category { get; set; }
+        [Display(Name = "Точка подключения")]
+        public string Point { get; set; }
+        [Display(Name = "Инв. №")]
+        public int InvNum { get; set; }
     }
     public class DelineationContext: DbContext 
     {
