dc0da076b39d380f676ac62919add9a84e35e4f8
Андрей Сергеич <asgoreglyad@gmail.com>
2022-04-03 19:53:46 +0300

    Added appsettings.local.json and .gitignore changed

--- Delineation/Program.cs
+++ Delineation/Program.cs
@@ -18,6 +18,10 @@ namespace Delineation
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile("appsettings.local.json", optional: true, reloadOnChange: true);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
